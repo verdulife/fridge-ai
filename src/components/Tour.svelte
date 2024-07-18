4ef04952cc6af41ad0e5159eb1d8178a13bcf8ab
verdulife <verdu@live.com>
2024-07-19 00:34:47 +0200

    Test

--- src/components/Tour.svelte
+++ src/components/Tour.svelte
@@ -53,6 +53,28 @@
 			<Button class="w-1/2 md:w-1/5" {nextTour}>EMPEZAR</Button>
 		</article>
 	</li>
+
+	<li class="relative w-full shrink-0 snap-center">
+		<article class="flex size-full flex-col items-center justify-center">
+			<div class="mb-40 flex flex-col items-center justify-center gap-4">
+				<h1 class="max-w-sm text-center text-4xl font-semibold">
+					hola, soy tu nevera digital inteligente
+				</h1>
+
+				<p class="max-w-sm text-center">
+					Olvídate para siempre lo que cocinar, si lo que cocinas es sano y equilibrado y si te hace
+					falta comprar algo en el super.
+				</p>
+
+				<p class="max-w-sm text-center">
+					Menú semanal, macronutrientes, kilocalorías, la lista de la compra, aquí lo tienes todo y
+					gratuito.
+				</p>
+			</div>
+
+			<Button class="w-1/2 md:w-1/5" {nextTour}>EMPEZAR</Button>
+		</article>
+	</li>
 </ul>
 
 <style>
