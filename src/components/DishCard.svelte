c78293bee73ea2c7f8852be5e637d0c38b9833a2
verdulife <verdu@live.com>
2024-07-31 17:13:13 +0200

    hide dishcard not generated on menus route

--- src/components/DishCard.svelte
+++ src/components/DishCard.svelte
@@ -85,73 +85,75 @@
 	}
 </script>
 
-<Box class="relative flex h-full min-h-48 w-80 flex-col items-start gap-4 p-4">
-	<header class="flex w-full items-center justify-between">
-		<span class="rounded-full bg-neutral-100 px-3 py-1 text-xs font-semibold dark:bg-neutral-950">
-			<slot />
-		</span>
-	</header>
+{#if dish?.label || $page.url.pathname !== '/menus'}
+	<Box class="relative flex h-full min-h-48 w-80 flex-col items-start gap-4 p-4">
+		<header class="flex w-full items-center justify-between">
+			<span class="rounded-full bg-neutral-100 px-3 py-1 text-xs font-semibold dark:bg-neutral-950">
+				<slot />
+			</span>
+		</header>
 
-	{#if dish?.label}
-		<Heading as="h3" class="text-lg">{dish.label}</Heading>
+		{#if dish?.label}
+			<Heading as="h3" class="text-lg">{dish.label}</Heading>
 
-		<footer
-			class="mt-auto flex w-full items-center justify-between border-t border-neutral-700/20 pt-4 dark:border-neutral-800"
-		>
-			<Text class="flex items-center gap-1 text-xs uppercase text-neutral-400">
-				<Time class="size-5" />
-				{!Number(dish.time_to_prepare) ? dish.time_to_prepare : `${dish.time_to_prepare} minutos`}
-			</Text>
+			<footer
+				class="mt-auto flex w-full items-center justify-between border-t border-neutral-700/20 pt-4 dark:border-neutral-800"
+			>
+				<Text class="flex items-center gap-1 text-xs uppercase text-neutral-400">
+					<Time class="size-5" />
+					{!Number(dish.time_to_prepare) ? dish.time_to_prepare : `${dish.time_to_prepare} minutos`}
+				</Text>
 
-			<aside class="flex items-center gap-2">
-				<Button click={openDialog} class="more_info_dish px-3 py-1">
-					<Plus class="size-5" />
-				</Button>
+				<aside class="flex items-center gap-2">
+					<Button click={openDialog} class="more_info_dish px-3 py-1">
+						<Plus class="size-5" />
+					</Button>
 
-				<Button
-					click={regenerateMeal}
-					class="generate_alternative_dish flex items-center justify-center px-3 py-1"
-				>
-					{#if !isLoading}
+					<Button
+						click={regenerateMeal}
+						class="generate_alternative_dish flex items-center justify-center px-3 py-1"
+					>
+						{#if !isLoading}
+							<Ai class="size-5" />
+						{:else}
+							<script src="https://cdn.lordicon.com/lordicon.js"></script>
+							<lord-icon
+								src="https://cdn.lordicon.com/jpgpblwn.json"
+								trigger="loop"
+								state="loop-line"
+								colors={`primary:${$UiPreferences.dark_mode ? UI_COLORS.btn_dark : UI_COLORS.btn_light}`}
+								style="width:20px;height:20px"
+							>
+							</lord-icon>
+						{/if}
+					</Button>
+				</aside>
+			</footer>
+		{:else}
+			<div class="absolute inset-0 flex size-full items-center justify-center">
+				{#if !meals_state[type]}
+					<Button
+						click={() => generateTodayMeal()}
+						class="flex items-center justify-center gap-1 px-3 py-1"
+					>
 						<Ai class="size-5" />
-					{:else}
-						<script src="https://cdn.lordicon.com/lordicon.js"></script>
-						<lord-icon
-							src="https://cdn.lordicon.com/jpgpblwn.json"
-							trigger="loop"
-							state="loop-line"
-							colors={`primary:${$UiPreferences.dark_mode ? UI_COLORS.btn_dark : UI_COLORS.btn_light}`}
-							style="width:20px;height:20px"
-						>
-						</lord-icon>
-					{/if}
-				</Button>
-			</aside>
-		</footer>
-	{:else}
-		<div class="absolute inset-0 flex size-full items-center justify-center">
-			{#if !meals_state[type]}
-				<Button
-					click={() => generateTodayMeal()}
-					class="flex items-center justify-center gap-1 px-3 py-1"
-				>
-					<Ai class="size-5" />
-					Generar
-				</Button>
-			{:else}
-				<script src="https://cdn.lordicon.com/lordicon.js"></script>
-				<lord-icon
-					src="https://cdn.lordicon.com/jpgpblwn.json"
-					trigger="loop"
-					state="loop-line"
-					colors={`primary:${$UiPreferences.dark_mode ? UI_COLORS.btn_dark : UI_COLORS.btn_light}`}
-					style="width:40px;height:40px"
-				>
-				</lord-icon>
-			{/if}
-		</div>
-	{/if}
-</Box>
+						Generar
+					</Button>
+				{:else}
+					<script src="https://cdn.lordicon.com/lordicon.js"></script>
+					<lord-icon
+						src="https://cdn.lordicon.com/jpgpblwn.json"
+						trigger="loop"
+						state="loop-line"
+						colors={`primary:${$UiPreferences.dark_mode ? UI_COLORS.btn_dark : UI_COLORS.btn_light}`}
+						style="width:40px;height:40px"
+					>
+					</lord-icon>
+				{/if}
+			</div>
+		{/if}
+	</Box>
+{/if}
 
 {#if dish?.label}
 	<DishDialog bind:dish bind:open />
